3ef2c831cd88ef6ecace4b460713508aa049f2bc
Franco Oneca <francooneca@gmail.com>
2026-09-22 17:34:48 -0300

    Codigo unidad creado
    Se creo un codigo universal que contiene datos generales de las unidades como el nombre, nivel, salud y daño

---
+++ Assets/Scripts/Unidad.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unidad : MonoBehaviour
+{
+    public string nombre;
+    public int nivelUnidad;
+    public int danio;
+    public int saludMaxima;
+    public int saludActual;
+}
